02916f7634d1cb136e0834904eb4ca21a1a7da4e
ivanivorontsov-sudo <ivanivorontsov@gmail.com>
2026-08-18 19:16:21 +1000

    Add MainActivity.kt

---
+++ app/src/main/java/com/example/snakegame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.snakegame
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(SnakeGameView(this))
+    }
+}
